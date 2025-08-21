db9a8103860dc90e52d680d18f94ddade31fd8ce
Muhsina Musthafa K P <muhsinamusthafakp26@gmail.com>
2025-08-21 13:05:27 +0530

    '1:05'

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,79 +1,53 @@
-import React from "react";
-import "../Styles/Product.css";
+import React, { act, useState } from "react";
+import "../Styles/Product.css"; 
+import { products } from "../data/product";
 
-const products = [
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p1.webp",
-    badge: "Sale",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p2.webp",
-    badge: "-20%",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p3.webp",
-    badge: "Sale",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p4.webp",
-    badge: "New",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p3.webp",
-    badge: "Sale",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p4.webp",
-    badge: "-30%",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p1.webp",
-    badge: "New",
-  },
-  {
-    title: "Product Title",
-    price: 16.0,
-    oldPrice: 20.0,
-    img: "/images/p2.webp",
-    badge: "-60%",
-  },
-];
+const tabList = [
+  { value: 'all', label: 'All' },
+  { value: 'sale', label: 'Sale' },
+  { value: 'bestseller', label: 'Best seller' },
+  { value: 'featured', label: 'Featured' },
+]
+
+
+        // <button className="active">Sale</button>
+       
 
 const Products = () => {
+
+  const [activeTab, setActiveTab] = useState('all')
+  const [productList, setProductList] = useState(products)
+  const [filterValue, setFilterValue] = useState(null)
+  const [searchKey, setSearchKey] = useState('')
+
+  const handleTabChange = (value)=> {
+     setActiveTab(value)
+     console.log(value, ":  value")
+     console.log(activeTab, ":  active tab")
+
+     if ( value !== 'all') setProductList(products.filter(product=> product[value] === true))
+     if ( value === 'all' ) setProductList(products)
+
+  }
+
   return (
     <section className="products-section">
       <h5>Check Our All Products</h5>
       <h2>OUR PRODUCTS</h2>
       <div className="filter-buttons">
-        <button className="active">All</button>
-        <button>Sale</button>
-        <button>Bestseller</button>
-        <button>Featured</button>
+        {
+          tabList.map((item, idx)=> <button 
+                                    className={activeTab===item.value ? 'active' : ''} 
+                                    key={idx}
+                                    onClick={()=> handleTabChange(item.value)}
+                                    >
+                                      {item.label}
+                                    </button>)
+        }
       </div>
 
       <div className="products-grid">
-        {products.map((product) => (
+        {productList.map((product) => (
           <div className="product-card">
             <div className="badge">{product.badge}</div>
             <img src={product.img} alt={product.title} />
---
+++ src/data/product.js
@@ -0,0 +1,90 @@
+export const products = [
+  {
+    id: 1,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p1.webp",
+    badge: "Sale",
+    sale: true,
+    bestseller: true,
+    featured: false
+  },
+  {
+    id: 2,
+    title: "featured product",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p2.webp",
+    badge: "-20%",
+    sale: false,
+    bestseller: true,
+    featured: true
+  },
+  {
+    id: 3,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p3.webp",
+    badge: "Sale",
+    sale: true,
+    bestseller: false,
+    featured: true
+  },
+  {
+    id: 4,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p4.webp",
+    badge: "New",
+    sale: true,
+    bestseller: false,
+    featured: false
+  },
+  {
+    id: 5,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p3.webp",
+    badge: "Sale",
+    sale: true,
+    bestseller: true,
+    featured: false
+  },
+  {
+    id: 6,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p4.webp",
+    badge: "-30%",
+    sale: true,
+    bestseller: true,
+    featured: false
+  },
+  {
+    id: 7,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p1.webp",
+    badge: "New",
+    sale: true,
+    bestseller: true,
+    featured: false
+  },
+  {
+    id: 8,
+    title: "Product Title",
+    price: 16.0,
+    oldPrice: 20.0,
+    img: "/images/p2.webp",
+    badge: "-60%",
+    sale: true,
+    bestseller: true,
+    featured: false
+  },
+];
